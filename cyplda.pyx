a7a4b716c24ad7fff8edc7c6357d78595055de14
Charles Liu <therealcharlesliu@gmail.com>
2015-12-04 11:48:48 -0500

    multithreading - incomplete

--- cyplda.pyx
+++ cyplda.pyx
@@ -1,18 +1,148 @@
 cimport numpy as np
 import numpy as np
 cimport cython
-from cython.parallel import prange, parallel
-from omp_defs cimport omp_lock_t, get_N_locks, free_N_locks
 from cython.operator cimport preincrement as inc, predecrement as dec
 from libc.stdlib cimport rand, RAND_MAX
 from libc.stdlib cimport malloc, free
 
 '''
-Collapsed Gibbs Sampling
+Initial topic sampling for Gibbs sampling
+'''
+@cython.boundscheck(False)
+cpdef void init_topics(int[:, ::1] documents, double[:, ::1] K_V, double[:, ::1] D_K, int[::1] sum_K, int[::1] curr_K) nogil:
+    cdef size_t i,j,k
+    # number of topics
+    cdef int K = K_V.shape[0]    
+    cdef int count = 0
+    cdef int topic = 0
+    for i in xrange(documents.shape[0]):
+        for j in xrange(documents.shape[1]):
+            for k in xrange(documents[i,j]):
+                topic = count % K
+                curr_K[count] = topic
+                inc(sum_K[topic])
+                inc(K_V[topic, j])
+                inc(D_K[i, topic])
+                inc(count) 
+
+'''
+Normalization with increment mu
+'''
+@cython.boundscheck(False)
+cpdef void normalize(double[:, ::1] arr, double mu) nogil:
+    cdef double cumulative_p
+    cdef size_t i, j
+    for i in xrange(arr.shape[0]):
+        cumulative_p = 0
+        for j in xrange(arr.shape[1]):
+            arr[i,j] += mu
+            cumulative_p += arr[i,j]
+        for j in xrange(arr.shape[1]):
+            arr[i,j] /= cumulative_p
+            
+'''
+Array methods in cython
+'''
+@cython.boundscheck(False)
+cpdef void copy(int[::1] sum_K, int[::1] t_sum_K) nogil:
+    cdef size_t i
+    for i in xrange(sum_K.shape[0]):
+        t_sum_K[i] = sum_K[i]
+        
+@cython.boundscheck(False)
+cpdef void subtract(int[::1] t_sum_K, int[::1] sum_K) nogil:
+    cdef size_t i
+    for i in xrange(sum_K.shape[0]):
+        t_sum_K[i] -= sum_K[i]
+        
+@cython.boundscheck(False)
+cpdef void add(int[::1] sum_K, int[::1] t_sum_K) nogil:
+    cdef size_t i
+    for i in xrange(sum_K.shape[0]):
+        sum_K[i] += t_sum_K[i]
+
+'''
+Single iteration of CGS with dynamic sampling
+- document and word start/step parameters to perform partial CGS
+'''
+@cython.boundscheck(False)
+cpdef void CGS_iter(int[:, ::1] documents, double[:, ::1] K_V, double[:, ::1] D_K, int[::1] sum_K, int[::1] curr_K, double alpha, double beta, int[:, :, ::1] sampling, double[::1] p_K, int[::1] uniq_K, int thread_num, int num_threads) nogil:
+    cdef size_t i,j,k,p
+    # number of documents
+    cdef int D = documents.shape[0]
+    # number of types
+    cdef int V = documents.shape[1]
+    # number of topics
+    cdef int K = K_V.shape[0]
+    cdef double cumulative_p, randnum
+    cdef int num_samples, topic
+    cdef int count = 0
+    
+    for i in xrange(D):
+        for j in xrange(V):
+            #check if document has type j, if so go to sampling procedure
+            if documents[i,j] > 0:
+                #check if document belongs to thread
+                if thread_num <= i and (i-thread_num)%num_threads == 0:
+                    num_samples = documents[i,j]
+                    if num_samples > 2:
+                        #more than 2 occurrences, so do dynamic sampling procedure
+                        cumulative_p = 0
+                        for p in xrange(num_samples):
+                            cumulative_p += sampling[i,j,p]
+                        #choose random number between 0 and sum of samples
+                        #if cumulative_p is 0, must initialize the sampling vector
+                        if cumulative_p == 0:
+                            sampling[i,j,num_samples-1] = 1
+                            cumulative_p = 1
+                        randnum = rand()/float(RAND_MAX)*cumulative_p
+                        num_samples = -1
+                        cumulative_p = 0
+                        while cumulative_p < randnum and num_samples < documents[i,j]:
+                            cumulative_p += sampling[i,j,inc(num_samples)]
+                    #only looping through num_samples times, reset uniq_k
+                    for k in xrange(K):
+                        uniq_K[k] = 0
+                    for k in xrange(num_samples):
+                        #retrieve current topic and decrement
+                        topic = curr_K[count]
+                        dec(sum_K[topic])
+                        dec(K_V[topic,j])
+                        dec(D_K[i,topic])
+                        
+                        cumulative_p = 0
+                        for p in xrange(K):
+                            cumulative_p += (D_K[i, p] + alpha)*(K_V[p,j] + beta)/(sum_K[p] + V*beta)
+                            p_K[p] = cumulative_p
+                        randnum = rand()/float(RAND_MAX)*cumulative_p
+                        topic = get_index(p_K, randnum, K)
+                        
+                        #assign new topic
+                        curr_K[count] = topic
+                        inc(sum_K[topic])
+                        inc(K_V[topic,j])
+                        inc(D_K[i, topic])
+                        inc(count)
+                        
+                        #increment index in uniq_k
+                        inc(uniq_K[topic])
+                    #make sure to keep count consistent with documents table
+                    count += documents[i,j] - num_samples
+                    #find number of unique topics used and increment index
+                    num_samples = 0
+                    for k in xrange(K):
+                        if uniq_K[k] != 0:
+                            inc(num_samples)
+                    inc(sampling[i,j,num_samples])  
+                else:
+                    count += documents[i,j]
+
+'''
+Initial Serial Collapsed Gibbs Sampling
 - Dynamic sampling (http://jmlr.csail.mit.edu/proceedings/papers/v13/xiao10a/xiao10a.pdf)
 '''
 @cython.boundscheck(False)
-cpdef void CGS(int[:, ::1] documents, double[:, ::1] K_V, double[:, ::1] D_K, int[::1] sum_K, int[::1] curr_K, double alpha, double beta, unsigned int n, int[:, :, ::1] sampling) nogil:
+cpdef void CGS(int[:, ::1] documents, double[:, ::1] K_V, double[:, ::1] D_K, int[::1] sum_K, int[::1] curr_K, double alpha, double beta, unsigned int n, int[:, :, ::1] sampling, double[::1] p_K, int[::1] uniq_K) nogil:
     cdef size_t i, j, k, it, p
     # number of documents
     cdef int D = documents.shape[0]
@@ -20,26 +150,21 @@ cpdef void CGS(int[:, ::1] documents, double[:, ::1] K_V, double[:, ::1] D_K, in
     cdef int V = documents.shape[1]
     # number of topics
     cdef int K = K_V.shape[0]
-    # malloc K doubles to store probability distribution
-    cdef double* p_K = <double*> malloc(K*sizeof(double))
-    # malloc K integers to store unique topics from sampling
-    cdef int* uniq_K = <int*> malloc(K*sizeof(int))
     cdef double cumulative_p
     cdef double randnum
-    cdef int num_samples
+    cdef int num_samples, count, topic    
         
     #initialize random topics to start
-    cdef int count = 0
-    cdef int topic = 0
-    for i in range(D):
-        for j in range(V):
-            for k in range(documents[i,j]):
+    count = 0
+    for i in xrange(documents.shape[0]):
+        for j in xrange(documents.shape[1]):
+            for k in xrange(documents[i,j]):
                 topic = count % K
                 curr_K[count] = topic
                 inc(sum_K[topic])
                 inc(K_V[topic, j])
                 inc(D_K[i, topic])
-                inc(count)
+                inc(count) 
     
     #proceed to Gibbs sampling
     for it in range(n):
@@ -98,9 +223,6 @@ cpdef void CGS(int[:, ::1] documents, double[:, ::1] K_V, double[:, ::1] D_K, in
                         if uniq_K[k] != 0:
                             inc(num_samples)
                     inc(sampling[i,j,num_samples])                    
-                    
-    free(p_K)
-    free(uniq_K)
     
     #normalize K_V and D_K
     for i in xrange(K):
@@ -116,11 +238,11 @@ cpdef void CGS(int[:, ::1] documents, double[:, ::1] K_V, double[:, ::1] D_K, in
             D_K[i,j] += alpha
             cumulative_p += D_K[i,j]
         for j in xrange(K):
-            D_K[i,j] /= cumulative_p
+            D_K[i,j] /= cumulative_p  
         
     
 @cython.boundscheck(False)
-cdef int get_index(double* p_K, double randnum, int K) nogil:
+cdef int get_index(double[::1] p_K, double randnum, int K) nogil:
     cdef int mink = 0
     cdef int maxk = K-1
     cdef int midk
@@ -131,3 +253,4 @@ cdef int get_index(double* p_K, double randnum, int K) nogil:
         else:
             maxk = midk
     return maxk
+    
--- plda.py
+++ plda.py
@@ -1,11 +1,14 @@
 import cyplda
 import numpy as np
+import threading
+import time
 
 class LDA:
-    def __init__(self, num_topics, iterations = 500, damping = 1):
+    def __init__(self, num_topics, iterations = 500, damping = 1, sync_interval = 1):
         self.num_topics = num_topics
         self.iterations = iterations
         self.damping = damping
+        self.sync_interval = sync_interval
         
     def set_topics(self, n):
         self.num_topics = n
@@ -13,7 +16,14 @@ class LDA:
     def set_iterations(self, i):
         self.iterations = i
         
-    def CGS(self, documents, alpha=None, beta=None):
+    def set_damping(self, d):
+        self.damping = d
+        
+    def set_sync_interval(self, s):
+        self.sync_interval = s
+        
+    #baseline serial cython CGS
+    def sCGS(self, documents, alpha=None, beta=None):
         if alpha is None or alpha <= 0:
             alpha = 50./self.num_topics
         if beta is None or beta <= 0:
@@ -29,8 +39,97 @@ class LDA:
         curr_K = np.zeros((np.sum(documents)), dtype=np.dtype("i"))
         # sampling distributions
         sampling = np.zeros((documents.shape[0], documents.shape[1], np.max(documents)), dtype=np.dtype("i"))
-        cyplda.CGS(documents, K_V, D_K, sum_K, curr_K, alpha, beta, self.iterations, sampling)
+        # vectors for probability and topic counts
+        p_K = np.zeros((self.num_topics), dtype=np.float)
+        uniq_K = np.zeros((self.num_topics), dtype=np.dtype("i"))
+        cyplda.CGS(documents, K_V, D_K, sum_K, curr_K, alpha, beta, self.iterations, sampling, p_K, uniq_K)
         self.K_V = K_V
         self.D_K = D_K
-        self.sum_K = sum_K
-        self.curr_K = curr_K
+        
+    def pCGS(self, documents, num_threads = 4, alpha=None, beta=None):
+        if alpha is None or alpha <= 0:
+            alpha = 50./self.num_topics
+        if beta is None or beta <= 0:
+            beta = 0.1
+            
+        # topic -> words distribution
+        K_V = np.zeros((self.num_topics,documents.shape[1]), dtype=np.float)
+        # documents -> topic distribution
+        D_K = np.zeros((documents.shape[0], self.num_topics), dtype=np.float)
+        # sum of types per topic
+        sum_K = np.zeros((self.num_topics), dtype=np.dtype("i"))
+        # current topic for ith word in corpus
+        curr_K = np.zeros((np.sum(documents)), dtype=np.dtype("i"))
+        # sampling distributions
+        sampling = np.zeros((documents.shape[0], documents.shape[1], np.max(documents)), dtype=np.dtype("i"))
+        
+        start = time.time()
+        cyplda.init_topics(documents, K_V, D_K, sum_K, curr_K)
+        init_end = time.time()
+        print "Init:{}".format(init_end - start)
+        #vector of threads
+        tList = [None]*num_threads
+        #update iterations on threads
+        updateCount = [0]
+        #copy iterations on threads
+        copyCount = [0]
+        #condition objects to synchronize iList
+        updateCondition = threading.Condition()
+        copyCondition = threading.Condition()
+        for i in range(num_threads):
+            tList[i] = threading.Thread(target=self.workerCGS, args=(updateCondition, copyCondition, updateCount, copyCount, i, num_threads, documents, K_V, D_K, sum_K, curr_K, alpha, beta, sampling))
+            tList[i].start()
+        for i in range(num_threads):
+            tList[i].join()
+            
+        cgs_end = time.time()
+        print "CGS:{}".format(cgs_end-init_end)
+        
+        cyplda.normalize(K_V, beta)
+        cyplda.normalize(D_K, alpha)           
+        
+        norm_end = time.time()
+        print "Norm:{}".format(norm_end - cgs_end)
+            
+        self.K_V = K_V
+        self.D_K = D_K
+        assert np.sum(sum_K) == np.sum(documents), "Not syncing correctly"
+        
+    def workerCGS(self, updateCondition, copyCondition, updateCount, copyCount, thread_num, num_threads, documents, K_V, D_K, sum_K, curr_K, alpha, beta, sampling):
+        # vectors for probability and topic counts
+        p_K = np.zeros((self.num_topics), dtype=np.float)
+        uniq_K = np.zeros((self.num_topics), dtype=np.dtype("i"))
+        
+        #create a copy of sum_K to work over
+        t_sum_K = sum_K.copy()
+        
+        for i in xrange(self.iterations):
+            cyplda.CGS_iter(documents, K_V, D_K, t_sum_K, curr_K, alpha, beta, sampling, p_K, uniq_K, thread_num, num_threads)
+            
+            if i%self.sync_interval == 0:
+                #must synchronize sum_K
+                cyplda.subtract(t_sum_K, sum_K)
+                #notify threads waiting on condition that you've completed this sync iteration
+                #threads will continue in wait loop until all threads have completed their sync iteration
+                with updateCondition:
+                    updateCount[0] += 1
+                    if updateCount[0]%num_threads == 0:
+                        updateCondition.notifyAll()
+                    
+                #wait for all threads to reach this point, then one at a time update sum_K
+                with updateCondition:
+                    while updateCount[0]%num_threads != 0:
+                        updateCondition.wait()
+                    cyplda.add(sum_K, t_sum_K)
+                    with copyCondition:
+                        copyCount[0] += 1
+                        if copyCount[0]%num_threads == 0:
+                            copyCondition.notifyAll()
+                    
+                #at this point need to wait for all threads to update sum_K with their changes
+                #once all threads reach this point it's safe to copy sum_K to t_sum_K
+                with copyCondition:
+                    while copyCount[0]%num_threads != 0:
+                        copyCondition.wait()
+                    cyplda.copy(sum_K, t_sum_K)
+            
