a18f591b204459e0d652327cebe91ec976ad4ca3
nicodri <nicodrizard@hotmail.com>
2015-12-04 22:04:32 -0500

    updates

--- Variational Inference/setup.py
+++ Variational Inference/setup.py
@@ -5,5 +5,7 @@ from Cython.Distutils import build_ext
 setup(
       cmdclass = {'build_ext': build_ext},
       ext_modules = [Extension('lda_vi_cython',
-                               ['lda_vi_cython.pyx'])]
+                               ['lda_vi_cython.pyx'],
+                               etra_compile_args=['-fopenmp'],
+                               extra_link_args=['-fopenmp'])]
       )
